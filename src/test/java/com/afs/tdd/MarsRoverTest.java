b376e95570fba690d60f5e9c02ae88c7cd1b0700
blank1024 <13380497335@163.com>
2022-07-21 20:26:11 +0800

    test: add test that when begin status is(2,2,E) and commands is L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -11,10 +11,22 @@ class MarsRoverTest {
         // given
         marsRover = new MarsRover(2, 2, Constant.N);
         // when
-        marsRover.executeCommands("M");
+        marsRover.executeCommands(Constant.M);
         // then
         assertEquals(2, marsRover.getxIndex());
         assertEquals(3, marsRover.getyIndex());
+        assertEquals(Constant.N, marsRover.getDirection());
+    }
+
+    @Test
+    void should_move_2_2_N_when_executeCommands_given_2_2_E_L() {
+        // given
+        marsRover = new MarsRover(2, 2, Constant.E);
+        // when
+        marsRover.executeCommands(Constant.L);
+        // then
+        assertEquals(2, marsRover.getxIndex());
+        assertEquals(2, marsRover.getyIndex());
         assertEquals(Constant.N, marsRover.getDirection());
     }
 }
